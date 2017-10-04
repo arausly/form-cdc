295139bccc638b9b5c5898437bbf01e95414a445
arausly <arausid@yahoo.com>
2017-10-04 21:40:13 +0100

    switched routes

--- app.js
+++ app.js
@@ -1,77 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('./db/config');
 const formModel = require('./db/formModel');
 const session = require('express-session');
 const mongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
 const morgan = require('morgan');
+const favicon = require('favicon');
+const mongoose = require('mongoose');
 
-const {PORT,SESSION_STORE,DB_STORE,JWT_SECRET} = require('./configjs');
 
+const app = express();
 
-var app = express();
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'index.html')));
 app.use(session({
-    key:'cdcForm.sid',
-    store:new mongoStore({
-        url:SESSION_STORE,
-        autoReconnect:true,
-        ttl:1000 * 60 * 60 * 24 * 2
-    }),
-    resave:true,
-    saveUninitialized:false,
-    secret:JWT_SECRET,
+  key:'cdcForm.sid',
+  resave:true,
+  saveUninitialized:false,
+  secret:'this_is_bad_practice',
+  store: new mongoStore({url:'mongodb://sessions:1234@ds129004.mlab.com:29004/cdc_form',
+  autoReconnect:true,
+  ttl:1000 * 60 * 60 * 24 * 2
+})
 }));
-app.use(flash);
-app.use(morgan('dev'));
-app.use(function(req,res,next){
-    res.locals.user = {};
-    next();
-})
+app.use(flash());
 
-app.get('/users',(req,res,next)=>{
-    formModel.find({})
-        .then(users =>{
-            if(!users) return Promise.reject();
-            res.status(200).send(users);
-        })
-        .catch(err => res.status(404).send())
-})
+mongoose.connect('mongodb://root:1234@ds129004.mlab.com:29004/cdc_form');
+mongoose.connection
+  .once('open',()=>{console.log('connected to database')})
+  .on('error',(err)=>{console.log('error',err)})
+  .on('disconnected',()=>{console.log('disconnected from database')});
 
-app.patch('/user/:_id',(req,res,next)=>{
-    let {_id} = req.params;
-    if(!id){
-        req.flash('notFound','record does not exist');
-        res.status(404).send(req.flash('notFound'));
-    }else{
-        formModel.findByIdAndUpdate({_id},{},{new:true})
-            .then((user)=>{
-                if(!user) return Promise.reject();
-                res.status(200).send();
-            })
-            .catch(err => res.status(404).send())
-    }
-})
+  mongoose.Promise = global.Promise;
+
+ // POST API >>> submit a user
 
 app.post('/submit',(req,res,next)=>{
-    let { name, sex, q1, q2, q3 } = req.body; 
-     let newData = new formModel({name,sex,ans1:q1,ans2:q2,an3:q3});
-     newData.save().then((user)=>{
-         res.redirect('/');
-         res.status(200).send(user);
-     });
+   let { name, sex, q1, q2, q3 } = req.body;
+   let userData = formModel({name, sex, ans1:q1, ans2:q2, ans3:q3 });
+   userData.save()
+       .then(user =>{
+           if(!user) return Promise.reject();
+           res.status(200).send(user);
+       })
+       .catch(err => console.error(err))
+})
+
+// POST API >>> get all users
+
+app.get('/users',(req,res,next)=>{
+    formModel.find({}).then(users =>{
+       if(!users) return Promise.reject();
+       res.status(200).send(users); 
+    }).catch(err => console.error(err));
+});
+
+// PATCH API >> update user data
+
+app.patch('/user/:_id',(req,res,next)=>{
+     const {_id} = req.params;
+    formModel.findById(_id)
+          .then(user =>{
+               if(!user) return Promise.reject();
+                res.redirect('/');
+                res.status(200).send(user);  
+          })
+          .catch(err => console.error(err));
 });
 
 app.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'index.html'));
+  res.sendFile(path.join(__dirname,'some.html'));
 });
 
-app.listen(PORT,(err)=>{
-    (err) ? console.log(err) :{};
-    console.log(`app is listening on port ${PORT}`);
+app.listen(3000,(err)=>{
+    err ? console.log(err) : console.log('app is running on 3000');
 })
-
--- configjs.js
+++
@@ -1,7 +0,0 @@
-
-   export const configFiles = {
-        JWT_SECRET:"627b93nnfefk22jkskndsk",
-        PORT:8080,
-        SESSION_STORE:"mongodb://sessions:1234@ds129004.mlab.com:29004/cdc_form",
-        DB_STORE:"mongodb://root:1234@ds129004.mlab.com:29004/cdc_form",
-    }
